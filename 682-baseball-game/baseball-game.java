5d5fa5b4629e564c54c588e50e6b461464bf00da
MANEESH_R_ADS <136969721+shoyo1234@users.noreply.github.com>
2026-02-13 18:39:23 +0530

    Time: 2 ms (91.58%) | Memory: 43.6 MB (63.15%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -2,17 +2,20 @@ class Solution {
     public int calPoints(String[] operations){
 
         // Stack to keep track of valid scores
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int sum = 0;
 
         for(String op : operations){
 
             if(op.equals("C")){
                 // Remove the last valid score
-                stack.pop();
+                sum  = sum - stack.pop();
             }
             else if(op.equals("D")){
                 // Double the last score and add it
-                stack.push(2 * stack.peek());
+                int newScore = 2*stack.peek();
+                stack.push(newScore);
+                sum = sum + newScore;
             }
             else if(op.equals("+")){
                 // Add last two scores
@@ -23,19 +26,15 @@ class Solution {
                 // Push back the removed score and the new one
                 stack.push(last_ele);
                 stack.push(newScore);
+                sum = sum + newScore;
             }
             else{
                 // Regular number just add to stack
-                stack.push(Integer.parseInt(op));
+                int new_ele = Integer.parseInt(op);
+                stack.push(new_ele);
+                sum = sum + new_ele;
             }
         }
-
-        // Calculate total score
-        int sum = 0;
-        for(int num : stack){
-            sum += num;
-        }
-
         return sum;
     }
 }
