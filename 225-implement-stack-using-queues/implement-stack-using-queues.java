4272cd219958c50bc3fdece32514bcec57b6a345
MANEESH_R <136969721+shoyo1234@users.noreply.github.com>
2026-03-19 16:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.45%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,40 +1,37 @@
 class MyStack {
 
+    // Using  queue to simulate stack behaviour
     private Queue<Integer> queue;
 
     public MyStack() {
         queue = new ArrayDeque<>();
-        
     }
     
+    // Push element onto stack
     public void push(int x) {
+
+        // Step 1: add element normally to queue
         queue.add(x);
-        for(int i = 1; i < queue.size(); i++){
+
+        // Step 2: move all previous elements behind the new element
+        int size = queue.size();
+        for (int i = 0; i < size - 1; i++) {
             queue.add(queue.remove());
         }
     }
     
+    // Remove and return the top element
     public int pop() {
         return queue.remove();
-        
     }
     
+    // Return top element without removing it
     public int top() {
         return queue.peek();
-        
     }
     
+    // Check whether stack is empty
     public boolean empty() {
         return queue.isEmpty();
-        
     }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
