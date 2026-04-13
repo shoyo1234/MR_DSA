f326144e13d74d70df3dce649c94411babb1babb
MANEESH_R <136969721+shoyo1234@users.noreply.github.com>
2026-04-13 22:19:01 +0530

    Time: 4 ms (47.04%) | Memory: 52.4 MB (22.84%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,25 +1,28 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-
+        //left and right are pointers used to construct the window
         int left = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-
-            // spend one flip if we see a zero
-            if (nums[right] == 0) {
-                k--;
+        //stores the max length of subarray
+        int max_length = 0;
+        //Used to check whether the zeros count exceeds k 
+        int zeros_count = 0;
+        for(int right = 0; right < nums.length; right++){
+            //if 0 spotted increase the count of zeros 
+            if(nums[right] == 0){
+                zeros_count = zeros_count+1;
             }
-
-            // if flips exceeded, shrink from left
-            if (k < 0) {
-                if (nums[left] == 0) {
-                    k++; // refund flip
+            //when zero count exceeds k move left, if arr[left] is zero reduce the count of zero 
+            while(zeros_count > k){
+                if(nums[left] == 0){
+                    zeros_count = zeros_count-1;
                 }
                 left++;
             }
+            //valid subarray length is stored
+            int current_length = right-left+1;
+            max_length = Math.max(max_length, current_length);
         }
+        return max_length;
 
-        // final window size
-        return nums.length - left;
     }
 }
